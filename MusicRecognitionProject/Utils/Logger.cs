ada1b1f580ef4dab1c9a7609c5058d9701cd39a0
Student <Student@DESKTOP-I9JU8B9>
2025-04-01 09:42:30 +0300

    added logger

---
+++ MusicRecognitionProject/Utils/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace MusicRecognitionProject.Utils
+{
+    public static class Logger
+    {
+        private static readonly object _locker = new object();
+
+        public static void LogInfo(object message)
+        {
+            var loggerDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Logger");
+            if (!Directory.Exists(loggerDirectory))
+            {
+                Directory.CreateDirectory(loggerDirectory);
+            }
+
+            var logFile = Path.Combine(loggerDirectory, "log.txt");
+            if (!File.Exists(logFile))
+            {
+                File.Create(logFile).Close();
+            }
+
+            lock (_locker)
+            {
+                using (StreamWriter writer = new StreamWriter(File.Open(logFile, FileMode.Append)))
+                {
+                    writer.WriteLine($"[{DateTime.Now}\t{Process.GetCurrentProcess().Id}]: {message}");
+                }
+            }
+        }
+    }
+}
